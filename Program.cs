41f6ed65f7811e4c1461a90586c998afced2c601
Piyush Singh <workspace.piyush01@gmail.com>
2025-09-08 11:30:47 +0530

    Initial Setup

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Serilog setup from config
+builder.Host.UseSerilog((context, services, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration)
+                 .ReadFrom.Services(services));
+
+// JWT config placeholder
+var jwtConfig = builder.Configuration.GetSection("Jwt");
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtConfig["Issuer"],
+            ValidAudience = jwtConfig["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes(jwtConfig["Key"]!))
+        };
+    });
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+// Swagger only if enabled
+if (builder.Configuration.GetSection("Swagger").GetValue<bool>("Enabled"))
+    builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Configuration.GetSection("Swagger").GetValue<bool>("Enabled"))
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
